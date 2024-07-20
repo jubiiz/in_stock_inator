eeb5dc6174c936a2755abf2c130e4cc842071cf8
jubiiz <jubiiz.audet@gmail.com>
2024-07-20 16:30:22 -0400

    Minor refactor.

--- src/stock_checker.py
+++ src/stock_checker.py
@@ -1,43 +1,55 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 
-# Path to the geckodriver binary
-gecko_driver_path = '/usr/local/bin/geckodriver'  # Adjust if necessary
+#https://www.facetofacegames.com/emrakul-the-world-anew-6-modern-horizons-3/
+#https://www.facetofacegames.com/the-one-ring-451-borderless-bundle-the-lord-of-the-rings-tales-of-middle-earth/
 
-# Set up the FirefoxDriver
-service = Service(gecko_driver_path)
-options = webdriver.FirefoxOptions()
-options.add_argument('--headless')
+LINKS_TO_CHECK = [
+    "https://www.facetofacegames.com/emrakul-the-world-anew-6-modern-horizons-3/",
+    "https://www.facetofacegames.com/the-one-ring-451-borderless-bundle-the-lord-of-the-rings-tales-of-middle-earth/",
+]
 
-driver = webdriver.Firefox(service=service, options=options)
 
-# Load the page
-driver.get('https://www.facetofacegames.com/emrakul-the-world-anew-6-modern-horizons-3/')
-#driver.get('https://www.facetofacegames.com/the-one-ring-451-borderless-bundle-the-lord-of-the-rings-tales-of-middle-earth/')
+def main():
+    driver = get_web_driver()
+    soup = get_soup_from_link(driver, "https://www.facetofacegames.com/emrakul-the-world-anew-6-modern-horizons-3/")
+    result = get_quantity_from_ftf_soup(soup)
+    res_as_int = int(result)
+    print(res_as_int)
 
-# Wait for the page to fully render (adjust timeout as needed)
-WebDriverWait(driver, 10)
 
-time.sleep(3)
+def get_web_driver() -> WebDriver:
+    gecko_driver_path = '/usr/local/bin/geckodriver'  # Adjust if necessary
+    service = Service(gecko_driver_path)
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--headless')
+    return webdriver.Firefox(service=service, options=options)
 
-# Get the page source after rendering
-page_source = driver.page_source
 
-# Parse the page source with BeautifulSoup
-soup = BeautifulSoup(page_source, 'html.parser')
+def get_soup_from_link(driver: WebDriver, link: str) -> BeautifulSoup:
+    driver.get(link)
+    WebDriverWait(driver, 10)
+    time.sleep(3)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.quit()
+    return soup
 
-# Close the WebDriver
-driver.quit()
 
-card_name = soup.find_all("h1", class_="productView-title")[0].contents[0]
-# print(card_name)
+def get_card_name_from_ftf_soup(soup: BeautifulSoup) -> str:
+    return soup.find_all("h1", class_="productView-title")[0].contents[0]
 
-result = soup.find_all("div", class_="form-field--stock")[0]
-result = result.find("span").contents[0]
-res_as_int = int(result)
-print(res_as_int)
+
+def get_quantity_from_ftf_soup(soup: BeautifulSoup) -> int:
+    result = soup.find_all("div", class_="form-field--stock")[0]
+    result = result.find("span").contents[0]
+    return int(result)
+
+if __name__ == "__main__":
+    main()
+
