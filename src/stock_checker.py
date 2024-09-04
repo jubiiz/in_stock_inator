13dda6ab10c7fe689a77ec011548e054ef8cc02c
jubiiz <jubiiz.audet@gmail.com>
2024-09-03 20:19:51 -0400

    Fixed profile problem

--- src/stock_checker.py
+++ src/stock_checker.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
@@ -17,24 +18,23 @@ LINKS_TO_CHECK = [
 
 
 def main():
-    print("main")
     driver = get_web_driver()
-    print("soup")
-    soup = get_soup_from_link(driver, "https://www.facetofacegames.com/emrakul-the-world-anew-6-modern-horizons-3/")
-    print("qtt")
-    result = get_quantity_from_ftf_soup(soup)
-    res_as_int = int(result)
-    print(res_as_int)
+    for url in LINKS_TO_CHECK:
+        result = ftf_availability(driver, url)
+        res_as_int = int(result)
+        print(res_as_int)
+
+
+def ftf_availability(driver: WebDriver, url: str) -> int:
+    soup = get_soup_from_link(driver, url)
+    return get_quantity_from_ftf_soup(soup)
 
 
 def get_web_driver() -> WebDriver:
     gecko_driver_path = '/usr/local/bin/geckodriver'  # Adjust if necessary
     service = Service(gecko_driver_path)
-    print("options")
     options = webdriver.FirefoxOptions()
-    print("firefox")
-    options.set_preference('profile', '/home/jubiiz/documents/code/in_stock_inator/firefox_setup/profile')  # Replace with the actual path
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     return webdriver.Firefox(service=service, options=options)
 
 
